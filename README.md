6af702deccccf3d6070dcdd0f17217425b9725cc
chase0213 <chase0213+dev@gmail.com>
2018-03-02 18:04:57 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# wareki-cli
+
+wareki-cli は、Go言語で作られた和暦と西暦の変換ツールです。
+
+## Installation
+
+
+
+## Usage
+
+### 西暦から和暦へ変換
+
+西暦から和暦への変換時には、コマンド `w` または `wareki` を指定して、その直後に `/` または `-` 区切りの日付を `YYYY/MM/DD` のフォーマットで入力します。
+
+```bash
+$ wareki-cli w 1989/02/13
+平成1年2月13日
+
+$ wareki-cli w 1989/01/07
+昭和64年1月7日
+```
+
+### 和暦から西暦へ変換
+
+和暦から西暦への変換時には、コマンド `s` または `seireki` を指定して、元号、年、月、日の順で入力します。
+
+```bash
+$ wareki-cli s 平成1年2月13日
+1989/2/13
+
+$ wareki-cli s 昭和六十四年一月七日
+1989/1/7
+```
+
+## Contributing
+
+このツールは、[wareki](https://github.com/chase0213/wareki)を基に作られており、このツールはコマンドラインパーサー以外のロジックを持ちません。
+コマンドラインへの機能追加、バグ修正等のコントリビューション以外は、[wareki](https://github.com/chase0213/wareki)にしていたたくようお願いします。
+
+具体的なコントリビュートの方法は、以下の手順を推奨しています。
+
+1. Fork it
+2. Create your feature branch (git checkout -b my-new-feature)
+3. Commit your changes (git commit -am 'Add some feature')
+4. Push to the branch (git push origin my-new-feature)
+5. Create new Pull Request
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	wrk "github.com/chase0213/wareki"
+	"github.com/urfave/cli"
+)
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "wareki"
+	app.Usage = ""
+	app.Version = "0.0.1"
+
+	// global options
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "wareki, w",
+			Usage: "-w ${YEAR}/${MONTH}/${DAY}",
+		},
+	}
+
+	// commands
+	app.Commands = []cli.Command{
+		{
+			Name:    "wareki",
+			Aliases: []string{"w"},
+			Usage:   "./wareki w ${YEAR}/${MONTH}/${DAY}",
+			Action:  SeirekiToWarekiAction,
+		},
+		{
+			Name:    "seireki",
+			Aliases: []string{"s"},
+			Usage:   "./wareki s ${WAREKI}${YEAR}年${MONTH}月${DAY}日",
+			Action:  WarekiToSeirekiAction,
+		},
+	}
+
+	app.Before = func(c *cli.Context) error {
+		return nil
+	}
+
+	app.After = func(c *cli.Context) error {
+		return nil
+	}
+
+	app.Run(os.Args)
+}
+
+func WarekiToSeirekiAction(c *cli.Context) {
+	var dateStr = ""
+	if len(c.Args()) > 0 {
+		dateStr = c.Args().First()
+	}
+
+	fmt.Printf("kokomade")
+	wareki, err := wrk.ParseWarekiString(dateStr)
+	if err != nil {
+		fmt.Printf("[Error] %s\n", err)
+	}
+
+	fmt.Printf("toottayo")
+	seireki, err := wareki.Seireki()
+	if err != nil {
+		fmt.Printf("[Error] %s\n", err)
+	}
+
+	fmt.Printf("%d年%d月%d日\n", seireki.Year, seireki.Month, seireki.Day)
+}
+
+func SeirekiToWarekiAction(c *cli.Context) {
+	var dateStr = ""
+	if len(c.Args()) > 0 {
+		dateStr = c.Args().First()
+	}
+
+	seireki, err := wrk.ParseSeirekiString(dateStr)
+	if err != nil {
+		fmt.Printf("[Error] %s\n", err)
+	}
+
+	wareki, err := seireki.Wareki()
+	if err != nil {
+		fmt.Printf("[Error] %s\n", err)
+	}
+
+	fmt.Printf("%s%d年%d月%d日\n", wareki.Name, wareki.Year, wareki.Month, wareki.Day)
+}
