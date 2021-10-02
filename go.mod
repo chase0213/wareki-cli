1010db6e92f485999e2409551a5159a4676ef6d3
chase0213 <chase0213+dev@gmail.com>
2021-10-02 17:55:46 +0900

    upgrade cli library version, and add "today" command

---
+++ go.mod
@@ -0,0 +1,9 @@
+module wareki-cli
+
+go 1.15
+
+require (
+	github.com/chase0213/wareki v0.0.0-20190401043020-2d0886f1e40a // indirect
+	github.com/dghubble/trie v0.0.0-20210609182954-9a58e577d803 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	wrk "github.com/chase0213/wareki/pkg/wareki"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
@@ -13,18 +14,24 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "wareki"
 	app.Usage = ""
-	app.Version = "0.0.1"
+	app.Version = "0.2.0"
 
 	// global options
 	app.Flags = []cli.Flag{
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "wareki, w",
 			Usage: "-w ${YEAR}/${MONTH}/${DAY}",
 		},
 	}
 
 	// commands
-	app.Commands = []cli.Command{
+	app.Commands = []*cli.Command{
+		{
+			Name:    "today",
+			Aliases: []string{"t"},
+			Usage:   "./wareki t",
+			Action:  TodayAction,
+		},
 		{
 			Name:    "wareki",
 			Aliases: []string{"w"},
@@ -50,44 +57,69 @@ func main() {
 	app.Run(os.Args)
 }
 
-func WarekiToSeirekiAction(c *cli.Context) {
+func TodayAction(c *cli.Context) error {
+	now := time.Now()
+	dateStr := fmt.Sprintf("%d/%d/%d", now.Year(), now.Month(), now.Day())
+
+	seireki, err := wrk.ParseSeirekiString(dateStr)
+	if err != nil {
+		fmt.Printf("[Error] %s\n", err)
+		return err
+	}
+
+	wareki, err := seireki.Wareki()
+	if err != nil {
+		fmt.Printf("[Error] %s\n", err)
+		return err
+	}
+
+	fmt.Printf("今日は %s%d年%d月%d日 です。\n", wareki.Name(), wareki.Year(), wareki.Month(), wareki.Day())
+
+	return nil
+}
+
+func WarekiToSeirekiAction(c *cli.Context) error {
 	var dateStr = ""
-	if len(c.Args()) > 0 {
+	if c.Args().Len() > 0 {
 		dateStr = c.Args().First()
 	}
 
 	wareki, err := wrk.ParseWarekiString(dateStr)
 	if err != nil {
 		fmt.Printf("[Error] %s\n", err)
-		return
+		return err
 	}
 
 	seireki, err := wareki.Seireki()
 	if err != nil {
 		fmt.Printf("[Error] %s\n", err)
-		return
+		return err
 	}
 
 	fmt.Printf("%d年%d月%d日\n", seireki.Year(), seireki.Month(), seireki.Day())
+
+	return nil
 }
 
-func SeirekiToWarekiAction(c *cli.Context) {
+func SeirekiToWarekiAction(c *cli.Context) error {
 	var dateStr = ""
-	if len(c.Args()) > 0 {
+	if c.Args().Len() > 0 {
 		dateStr = c.Args().First()
 	}
 
 	seireki, err := wrk.ParseSeirekiString(dateStr)
 	if err != nil {
 		fmt.Printf("[Error] %s\n", err)
-		return
+		return err
 	}
 
 	wareki, err := seireki.Wareki()
 	if err != nil {
 		fmt.Printf("[Error] %s\n", err)
-		return
+		return err
 	}
 
 	fmt.Printf("%s%d年%d月%d日\n", wareki.Name(), wareki.Year(), wareki.Month(), wareki.Day())
+
+	return nil
 }
